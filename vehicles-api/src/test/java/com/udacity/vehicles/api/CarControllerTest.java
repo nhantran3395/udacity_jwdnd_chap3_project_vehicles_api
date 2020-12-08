0a1fade74539d012daffaad797a6265355db24ae
tranthanhnhan3395 <thanhnhan33.95@gmail.com>
2020-12-08 17:03:35 +0700

    add test for get cars, delete car, get car by id

--- vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
+++ vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
@@ -96,6 +96,9 @@ public class CarControllerTest {
          *   the whole list of vehicles. This should utilize the car from `getCar()`
          *   below (the vehicle will be the first in the list).
          */
+        mvc.perform(
+                get(new URI("/cars")))
+                .andExpect(status().isOk());
 
     }
 
@@ -109,6 +112,10 @@ public class CarControllerTest {
          * TODO: Add a test to check that the `get` method works by calling
          *   a vehicle by ID. This should utilize the car from `getCar()` below.
          */
+        mvc.perform(
+                get(new URI("/cars/1")))
+                .andExpect(status().isOk());
+
     }
 
     /**
@@ -122,6 +129,10 @@ public class CarControllerTest {
          *   when the `delete` method is called from the Car Controller. This
          *   should utilize the car from `getCar()` below.
          */
+        mvc.perform(
+                delete(new URI("/cars/1")))
+                .andExpect(status().isNoContent());
+
     }
 
     /**
