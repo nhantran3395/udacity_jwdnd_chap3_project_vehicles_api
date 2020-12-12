cac00e0d0071f84f84e52f4251a219fbaa137c74
tranthanhnhan3395 <thanhnhan33.95@gmail.com>
2020-12-12 10:22:50 +0700

    clear all to do comments for cleaner look

--- vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
+++ vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
@@ -100,11 +100,6 @@ public class CarControllerTest {
      */
     @Test
     public void listCars() throws Exception {
-        /**
-         * TODO: Add a test to check that the `get` method works by calling
-         *   the whole list of vehicles. This should utilize the car from `getCar()`
-         *   below (the vehicle will be the first in the list).
-         */
         mvc.perform(
                 get(new URI("/cars")))
                 .andExpect(status().isOk());
@@ -117,10 +112,6 @@ public class CarControllerTest {
      */
     @Test
     public void findCar() throws Exception {
-        /**
-         * TODO: Add a test to check that the `get` method works by calling
-         *   a vehicle by ID. This should utilize the car from `getCar()` below.
-         */
         mvc.perform(
                 get(new URI("/cars/1")))
                 .andExpect(status().isOk());
@@ -133,11 +124,6 @@ public class CarControllerTest {
      */
     @Test
     public void deleteCar() throws Exception {
-        /**
-         * TODO: Add a test to check whether a vehicle is appropriately deleted
-         *   when the `delete` method is called from the Car Controller. This
-         *   should utilize the car from `getCar()` below.
-         */
         mvc.perform(
                 delete(new URI("/cars/1")))
                 .andExpect(status().isNoContent());
